61f5fbc3c4fd8a74e72c6bd8a9e0fff970ca58f2
Binh Nguyen <binh.nt@fiberblock.io>
2022-04-27 11:04:13 +0700

    build success

--- src/abis/index.ts
+++ src/abis/index.ts
@@ -1,19 +1,4 @@
 import MultiCallAbi from "./multicall.json";
 import Erc20Abi from "./erc20.json";
-import LendingPoolAbi from "./LendingPool.json";
-import LendingPoolConfigAbi from "./LendingPoolConfig.json";
-import LendingPoolDataProviderAbi from "./LendingPoolDataProvider.json";
-import WETHGatewayAbi from "./WETHGateway.json";
-import LendingPoolAddressProviderAbi from "./LendingPoolAddressProvider.json";
-import PriceOracle from "./AaveOracle.json";
 
-export {
-  MultiCallAbi,
-  Erc20Abi,
-  LendingPoolAbi,
-  LendingPoolConfigAbi,
-  LendingPoolDataProviderAbi,
-  WETHGatewayAbi,
-  LendingPoolAddressProviderAbi,
-  PriceOracle,
-};
+export { MultiCallAbi, Erc20Abi };
--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -2,15 +2,7 @@ import { ethers } from "ethers";
 import type { Signer } from "ethers";
 import { getSimpleRpcProvider } from "./provider";
 import type { CHAIN } from "@/types/chain.type";
-import {
-  Erc20Abi,
-  MultiCallAbi,
-  LendingPoolAbi,
-  LendingPoolConfigAbi,
-  LendingPoolDataProviderAbi,
-  PriceOracle,
-  WETHGatewayAbi,
-} from "@/abis";
+import { Erc20Abi, MultiCallAbi } from "@/abis";
 import { MULTICALL_CONTRACT } from "@/constants/contracts";
 
 export const getContract = (
@@ -33,49 +25,4 @@ export const getErc20Contract = (
   signer?: Signer
 ) => {
   return getContract(Erc20Abi, tokenAddress, chainId, signer);
-};
-
-export const getLendingPoolContract = (
-  chainId: CHAIN,
-  LendingPoolAddress: string,
-  signer?: Signer
-) => {
-  return getContract(LendingPoolAbi, LendingPoolAddress, chainId, signer);
-};
-
-export const getLendingPoolDataProviderContract = (
-  chainId: CHAIN,
-  LendingPoolDataProviderAddress: string,
-  signer?: Signer
-) => {
-  return getContract(
-    LendingPoolDataProviderAbi,
-    LendingPoolDataProviderAddress,
-    chainId,
-    signer
-  );
-};
-
-export const getLendingPoolConfigContract = (
-  chainId: CHAIN,
-  address: string,
-  signer?: Signer
-) => {
-  return getContract(LendingPoolConfigAbi, address, chainId, signer);
-};
-
-export const getPriceOracleContract = (
-  chainId: CHAIN,
-  address: string,
-  signer?: Signer
-) => {
-  return getContract(PriceOracle, address, chainId, signer);
-};
-
-export const getEthGatewayContract = (
-  chainId: CHAIN,
-  address: string,
-  signer?: Signer
-) => {
-  return getContract(WETHGatewayAbi, address, chainId, signer);
 };
